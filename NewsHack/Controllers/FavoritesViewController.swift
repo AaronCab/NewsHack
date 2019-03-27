55939f75c4e3e875425fb7c3b07944e64144a288
AaronCab <Aaron@Pursuits-MacBook-Pro-17.local>
2019-03-27 18:18:35 -0400

    added view controllers

---
+++ NewsHack/Controllers/FavoritesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesViewController.swift
+//  NewsHack
+//
+//  Created by Aaron Cabreja on 3/27/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ NewsHack/Controllers/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  NewsHack
+//
+//  Created by Aaron Cabreja on 3/27/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
