8bfea1f80a35476e6759d25e5b5a2c7d4731a0ec
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-03-27 19:38:59 -0400

    created functions for search bar and table view

--- NewsHack/Controllers/CategoriesViewController.swift
+++ NewsHack/Controllers/CategoriesViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class CategoriesViewController: UIViewController {
+    //MARK: - Outlets and Properties
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-    
+    private var onlineArticles = [ArticleWrapper](){
+        didSet{
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    //MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupDataSoruceAndDelegate()
+        
     }
+    private func setupDataSoruceAndDelegate(){
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+//MARK: - Extentions
+//MARK: - UITableViewDelegate, UITableViewDataSource
+extension CategoriesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+//MARK: - UISearchBarDelegate
+extension CategoriesViewController: UISearchBarDelegate {
     
 }
